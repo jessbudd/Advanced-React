910c8d10ff12f177efd435ab86e418051b4f99cc
Jess <feralfp@gmail.com>
2021-02-06 16:56:27 +0800

    add Header

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,14 +1,28 @@
 import Link from "next/link";
+import styled from "styled-components";
+import Nav from "./Nav";
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  a {
+    color: white;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
       <div className="bar">
-        <Link href="/">Sick fits</Link>
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
+      <Nav />
     </header>
   );
 }
