3b2293b699e94c459adfc436c46eb2e9716e89d4
Jess <feralfp@gmail.com>
2021-02-12 13:01:11 +0800

    feat: add user schema and auth

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './Schemas/User';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sickfits-tutorial';
@@ -8,6 +10,16 @@ const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in
   secret: process.env.COOKIE_SECRET,
 };
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identiftyField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add initial roles here
+  },
+});
 
 export default config({
   // @ts-ignore
@@ -24,6 +36,7 @@ export default config({
   },
   lists: createSchema({
     // Schema items go in here
+    User,
   }),
   ui: {
     // TODO: change this for rules
